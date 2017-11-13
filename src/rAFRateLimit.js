c40da9bc011dee30d8b1f8018fc1fa6dfe396def
Aeoril <aeoril@ic3dimensions.com>
2017-11-13 00:19:38 -0600

    Added argument type check

--- src/rAFRateLimit.js
+++ src/rAFRateLimit.js
@@ -5,6 +5,10 @@
 function rAFRateLimit (func) {
   'use strict';
 
+  if (typeof func !== 'function') {
+    throw new TypeError('A single argument of type "function" is required.')
+  }
+
   let rAFId = null;
   let wrapperArgs;
 
