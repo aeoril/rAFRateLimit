edd0029b1ed2dc54cbd60e150938838dc390621b
Aeoril <aeoril@ic3dimensions.com>
2017-11-12 19:23:50 -0600

    Reworiking

--- src/rAFRateLimit.js
+++ src/rAFRateLimit.js
@@ -7,14 +7,15 @@ function rAFRateLimit (func) {
 
   var rAFId = null;
 
-  return function (...args) {
-    if (rAFId) {
-      window.cancelAnimationFrame(rAFId);
-    }
+  return function (args) {
 
-    rAFId = window.requestAnimationFrame(function (timestamp) {
-      func(...args, timestamp);
-      rAFId = null;
-    });
+    func.args = args;
+
+    if (rAFId === null) {
+      rAFId = window.requestAnimationFrame(function (timestamp) {
+        func(timestamp);
+        rAFId = null;
+      });
+    }
   };
 }
